e12928b5ee1a26e309e1c6f31e702e7a9005e31d
Jia Wu <wujia123@yahoo.com>
2008-10-30 22:46:48 -0400

    added maxp table

--- example.rb
+++ example.rb
@@ -2,3 +2,4 @@ $LOAD_PATH << "#{File.dirname(__FILE__)}/lib"
 require "ttfunk"
 
 file = TTFunk::File.new("data/fonts/comicsans.ttf")
+p file.maxp.num_glyphs
--- lib/ttfunk.rb
+++ lib/ttfunk.rb
@@ -33,7 +33,7 @@ class TTFunk
     end
     
     attr_reader :directory
-    has_tables :head, :hhea, :name
+    has_tables :head, :hhea, :name, :maxp
   end
   
   class Table 
@@ -74,7 +74,8 @@ class TTFunk
         @advance_width_max = data.unpack("n")
         
         data = fh.read(22)
-        @min_left_side_bearing, @min_right_side_bearing, @x_max_extent, @caret_slope_rise, @caret_slope_run,
+        @min_left_side_bearing, @min_right_side_bearing, @x_max_extent, 
+        @caret_slope_rise, @caret_slope_run,
         @caret_offset, _, _, _, _, @metric_data_format =
         data.unpack("n11").map {|e| to_signed(e) }
         
@@ -83,6 +84,20 @@ class TTFunk
       end
     end
     
+    class Maxp < Table
+      def initialize(fh,info)
+        fh.pos  = info[:offset]
+        @length = info[:length]
+        data    = fh.read(@length)
+        @version, @num_glyphs, @max_points, @max_contours, 
+        @max_component_points,@max_component_contours, @max_zones, 
+        @max_twilight_points, @max_storage, @max_function_defs, 
+        @max_instruction_defs,@max_stack_elements, 
+        @max_size_of_instructions, @max_component_elements, 
+        @max_component_depth = data.unpack("Nn14")
+      end
+    end    
+        
     class Name < Table
       def initialize(fh,info)
         fh.pos = info[:offset]
