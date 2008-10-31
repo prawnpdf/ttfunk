ea0f7f8095290eda8daee08aa7c5da94dd1d51f0
Gregory Brown <gregory.t.brown@gmail.com>
2008-10-30 21:53:11 -0400

    We got the funk. We got the funk (names)

--- example.rb
+++ example.rb
@@ -1,7 +1,4 @@
 $LOAD_PATH << "#{File.dirname(__FILE__)}/lib"
 require "ttfunk"
 
-file = TTFunk::File.new("data/fonts/DejaVuSans.ttf")
-#p [:x_min, :y_min, :x_max, :y_max].map { |x| file.head.send(x) }
-p file.hhea.length
-p [:ascent, :descent].map {|x| file.hhea.send(x) }
+file = TTFunk::File.new("data/fonts/comicsans.ttf")
