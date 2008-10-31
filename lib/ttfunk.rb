accd63594e0974f5a2ce442e74e3ccb4bcc89fa2
Jia Wu <wujia123@yahoo.com>
2008-10-30 21:05:50 -0400

    added hhea table

--- lib/ttfunk.rb
+++ lib/ttfunk.rb
@@ -62,6 +62,25 @@ class TTFunk
     
     class Hhea < Table
       def initialize(fh,info)
+        fh.pos = info[:offset]
+        @length = info[:length]
+        data   = fh.read(4)
+        @version = data.unpack("N")
+        
+        data = fh.read(6)
+        @ascent, @descent, @line_gap= data.unpack("n3").map {|e| to_signed(e) } 
+        
+        data = fh.read(2) 
+        @advance_width_max = data.unpack("n")
+        
+        data = fh.read(22)
+        @min_left_side_bearing, @min_right_side_bearing, @x_max_extent, @caret_slope_rise, @caret_slope_run,
+        @caret_offset, _, _, _, _, @metric_data_format =
+        data.unpack("n11").map {|e| to_signed(e) }
+        
+        data = fh.read(2)
+        @num_of_long_hor_metrics = data.unpack("n")
+        
       end
     end
     
