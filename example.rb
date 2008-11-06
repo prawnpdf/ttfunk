da929fa79fd18e2c677c13d79f703ec7b7ea3110
Gregory Brown <gregory.t.brown@gmail.com>
2008-11-06 15:53:21 -0500

    Now we're stuck :-/

--- example.rb
+++ example.rb
@@ -1,5 +1,5 @@
 $LOAD_PATH << "#{File.dirname(__FILE__)}/lib"
 require "ttfunk"
 
-file = TTFunk::File.new("data/fonts/gkai00mp.ttf")
-p file.hmtx.lsb
+file = TTFunk::File.new("data/fonts/comicsans.ttf")
+file.cmap.formats[0]
--- lib/ttfunk.rb
+++ lib/ttfunk.rb
@@ -1,3 +1,15 @@
+class Array
+  alias_method :original_index, :index
+  
+  def index(*a)
+    if block_given?
+      (0...size).find { |e| yield(self[e]) }
+    else
+      original_index(*a)
+    end
+  end
+end
+
 class TTFunk
   class File
     def initialize(file)
@@ -37,10 +49,77 @@ class TTFunk
     end
     
     attr_reader :directory
-    has_tables :head, :hhea, :name, :maxp, :hmtx
+    has_tables :head, :hhea, :name, :maxp, :hmtx, :cmap
   end
   
   class Table 
+    
+    class Cmap < Table
+      def initialize(fh, font, info)
+        @file = fh
+        @file.pos = info[:offset]
+        
+        @version, @num_tables = @file.read(4).unpack("n2")
+        
+        process_subtables(info[:offset])
+      end
+      
+      def process_subtables(table_start)
+        @sub_tables = {}
+        @formats = {}
+        @num_tables.times do
+          platform_id, encoding_id, offset = @file.read(8).unpack("n2N")
+          @sub_tables[[platform_id, encoding_id]] = offset
+        end
+        
+        @sub_tables.each do |ident, offset|
+          @file.pos = table_start + offset
+          format = @file.read(2).unpack("n").first 
+          case format
+          when 0
+            @file.read(4) # skip length, language for now
+            glyph_ids = @file.read(256).unpack("C256")
+            @formats[0] = glyph_ids
+          when 4
+            length, language = @file.read(4).unpack("n2")
+            segcount_x2, search_range, entry_selector, range_shift = 
+              @file.read(8).unpack("n4")
+            end_count = @file.read(segcount_x2).unpack("n#{segcount_x2 / 2}")
+            
+            @file.read(2) # skip reserved value
+            
+            start_count = @file.read(segcount_x2).unpack("n#{segcount_x2 / 2}")
+            id_delta = @file.read(segcount_x2).unpack("n#{segcount_x2 / 2}").
+                         map { |e| to_signed(e) }
+            id_range_offset = @file.read(segcount_x2).unpack("n#{segcount_x2 / 2}")
+
+            remaining_shorts = (@file.pos - table_start) / 2
+            glyph_ids = @file.read(remaining_shorts*2).unpack("n#{remaining_shorts}")
+            
+            ######
+            
+            (0..1000).each do |char|
+              end_index = end_count.index { |e| e >= char }
+              if start_count[end_index] <= char
+                if id_range_offset[end_index] == 0
+                 # p [char + id_delta[end_index], char]
+                else
+                 gindex = id_range_offset[(id_range_offset[end_index] / 2) + (char - start_count[end_index] )]
+                 p [gindex ? glyph_ids[gindex] : 0, char]
+                end
+              else
+                0
+              end
+            end
+            
+            ######
+            
+          else
+            warn "TTFunk: Format #{format} not implemented, skipping"
+          end
+        end 
+      end
+    end
     
     class Head < Table
       def initialize(fh, font, info)
@@ -105,7 +184,6 @@ class TTFunk
       end
     end
         
-    
     class Maxp < Table
       def initialize(fh, font, info)
         fh.pos  = info[:offset]
