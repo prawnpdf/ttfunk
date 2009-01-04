0f478628eddafb3a786d27b33937416ed97bf8a2
Jamis Buck <jamis@37signals.com>
2009-01-03 20:30:31 -0700

    Forgot to include this file in a previous commit

---
+++ lib/ttfunk/table/simple.rb
@@ -0,0 +1,14 @@
+require 'ttfunk/table'
+
+module TTFunk
+  class Table
+    class Simple < Table
+      attr_reader :tag
+
+      def initialize(file, tag)
+        @tag = tag
+        super(file)
+      end
+    end
+  end
+end
