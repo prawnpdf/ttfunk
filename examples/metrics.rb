698f2d93a3353ab970ec7c66bbc68c42f5f37c46
Matjaz Gregoric <mtyaka@gmail.com>
2018-02-06 10:22:35 +0100

    Remove unnecessary utf-8 encoding comments.

--- examples/metrics.rb
+++ examples/metrics.rb
@@ -1,5 +1,3 @@
-# encoding: utf-8
-
 require_relative '../lib/ttfunk'
 
 def character_lookup(file, character)
--- spec/integration/collection_spec.rb
+++ spec/integration/collection_spec.rb
@@ -1,5 +1,3 @@
-# coding: utf-8
-
 require 'spec_helper'
 
 describe TTFunk::Collection, '::open' do
