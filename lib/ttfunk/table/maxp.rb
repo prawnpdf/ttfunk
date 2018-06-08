120102a51c8a2ed9f39a6a10c4a68e81cd9c5d5b
Cameron Dutro <camertron@gmail.com>
2018-06-07 18:39:31 -0700

    Refactor the maxp table
    * Gate on version information instead of trying to parse out potentially missing fields.

--- lib/ttfunk/table/maxp.rb
+++ lib/ttfunk/table/maxp.rb
@@ -24,27 +24,33 @@ module TTFunk
       def self.encode(maxp, mapping)
         ''.tap do |table|
           num_glyphs = mapping.length
-          table <<
-            [maxp.version].pack('N') <<
-            [
-              num_glyphs, maxp.max_points, maxp.max_contours,
-              maxp.max_component_points, maxp.max_component_contours,
-              maxp.max_zones, maxp.max_twilight_points, maxp.max_storage,
+          table << [maxp.version, num_glyphs].pack('Nn')
+
+          if maxp.version == 0x10000
+            table << [
+              maxp.max_points, maxp.max_contours, maxp.max_component_points,
+              maxp.max_component_contours, maxp.max_zones,
+              maxp.max_twilight_points, maxp.max_storage,
               maxp.max_function_defs, maxp.max_instruction_defs,
               maxp.max_stack_elements, maxp.max_size_of_instructions,
               maxp.max_component_elements, maxp.max_component_depth
             ].pack('n*')
+          end
         end
       end
 
       private
 
       def parse!
-        @version, @num_glyphs, @max_points, @max_contours,
-          @max_component_points, @max_component_contours, @max_zones,
-          @max_twilight_points, @max_storage, @max_function_defs,
-          @max_instruction_defs, @max_stack_elements, @max_size_of_instructions,
-          @max_component_elements, @max_component_depth = read(length, 'Nn*')
+        @version, @num_glyphs = read(6, 'Nn')
+
+        if @version == 0x10000
+          @max_points, @max_contours, @max_component_points,
+            @max_component_contours, @max_zones, @max_twilight_points,
+            @max_storage, @max_function_defs, @max_instruction_defs,
+            @max_stack_elements, @max_size_of_instructions,
+            @max_component_elements, @max_component_depth = read(28, 'Nn*')
+        end
       end
     end
   end
