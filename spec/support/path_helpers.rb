ac741143427735a32c3faf2f0d1b81ba5c759676
Alexander Mankuta <alex@pointless.one>
2019-12-06 11:12:05 +0200

    Fix code style

--- spec/support/path_helpers.rb
+++ spec/support/path_helpers.rb
@@ -5,7 +5,7 @@ module PathHelpers
     base_path = File.expand_path(File.dirname(__FILE__) + '/../fonts')
     valid_filename = File.join(base_path, "#{name}.#{ext}")
     if File.file?(valid_filename)
-      return valid_filename
+      valid_filename
     else
       raise ArgumentError, "#{valid_filename} not found"
     end
