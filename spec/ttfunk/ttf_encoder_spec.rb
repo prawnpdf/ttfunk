901f7a2d95c16514380c83520d1f41641bb1ae04
Alexander Mankuta <alex@pointless.one>
2020-01-17 12:02:52 +0200

    Fix maxp table encoding
    A wrong field size wash chosen for maxPoints. This shifted the rest of
    the fields and rendered the table invalid.

--- spec/ttfunk/ttf_encoder_spec.rb
+++ spec/ttfunk/ttf_encoder_spec.rb
@@ -64,7 +64,7 @@ RSpec.describe TTFunk::TTFEncoder do
 
       # verified via the Font-Validator tool at:
       # https://github.com/HinTak/Font-Validator
-      expect(checksum).to eq(0xEAB69D71)
+      expect(checksum).to eq(0xF1AA96AB)
     end
   end
 end
